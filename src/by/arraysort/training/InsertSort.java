dfec187fe97fb5b7a9035c164be971e322dc0c64
Pavel Ivanouski <pavel_85_67@mail.ru>
2021-09-01 16:19:12 +0200

    Insertion Sort

--- src/by/arraysort/training/InsertSort.java
+++ src/by/arraysort/training/InsertSort.java
@@ -1,4 +1,24 @@
 package by.arraysort.training;
 
+import java.util.Arrays;
+
 public class InsertSort {
+    public static void main(String[] args) {
+        int[] array = {10, 2, 10, 3, 1, 2, 5};
+        System.out.println(Arrays.toString(array));
+        for (int left = 0; left < array.length; left++) {
+            int value = array[left];
+            int i = left - 1;
+            for (; i >= 0; i--) {
+                if (value < array[i]) {
+                    array[i + 1] = array[i];
+                } else {
+                    break;
+                }
+            }
+            array[i + 1] = value;
+        }
+        System.out.println(Arrays.toString(array));
+    }
+
 }
